011b1d7b3ff82a40f74ce4d6b1ff70ff104c9594
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-16 14:24:16 +0500

    Update basic_functions.py

--- functions/basic_functions.py
+++ functions/basic_functions.py
@@ -1,1 +1,18 @@
+def my_function():
+  print("Hello from a function")
 
+
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
+def get_greeting():
+  return "Hello from a function"
+message = get_greeting()
+print(message)
+
+def get_greeting():
+  return "Hello from a function"
+print(get_greeting())
