472f084e0ae715c26f40d2734482bb1c70addfdb
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-17 17:29:13 +0500

    Create function_arguments.py

---
+++ functions/function_arguments.py
@@ -0,0 +1,17 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function(fname):
+  print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+def my_function(animal, name):
+  print("I have a", animal)
+  print("My", animal + "'s name is", name)
+my_function(animal = "dog", name = "Buddy")
