1d34ccb080e92049700146b064637cf28c4ee615
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-22 21:28:29 +0500

    Create class_methods.py

---
+++ classes/class_methods.py
@@ -0,0 +1,22 @@
+class Student:
+    def hello(self):
+        print("Hello!")
+
+student1 = Student()
+student1.hello()
+
+
+class Dog:
+    def sound(self):
+        print("Woof!")
+
+dog1 = Dog()
+dog1.sound()
+
+
+class Calculator:
+    def add(self, a, b):
+        print(a + b)
+
+calc = Calculator()
+calc.add(5, 4)
