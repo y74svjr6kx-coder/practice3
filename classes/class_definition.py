27a7926d16908fcd2acd67c14586999f62271e71
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-22 21:27:45 +0500

    Create class_definition.py

---
+++ classes/class_definition.py
@@ -0,0 +1,19 @@
+class Student:
+    name = "Dilnara"
+
+student1 = Student()
+print(student1.name)
+
+
+class Car:
+    brand = "Toyota"
+
+car1 = Car()
+print(car1.brand)
+
+
+class Country:
+    name = "Kazakhstan"
+
+country1 = Country()
+print(country1.name)
