9601296cb37861eb63e21a83cd58d6fde43a3aa5
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-22 21:23:34 +0500

    Create return_values.py

---
+++ functions/return_values.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+print(add(4, 5))
+
+def square(number):
+    return number * number
+
+print(square(6))
+
+
+def greeting(name):
+    return "Hello " + name
+
+message = greeting("Dilnara")
+print(message)
