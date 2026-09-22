817506116c14ba5e79466f46cc052206968fbd3d
Dilnara <y74svjr6kx@privaterelay.appleid.com>
2026-09-22 21:28:51 +0500

    Create class_variables.py

---
+++ classes/class_variables.py
@@ -0,0 +1,28 @@
+class Student:
+    school = "AITU"
+
+student1 = Student()
+student2 = Student()
+
+print(student1.school)
+print(student2.school)
+
+
+class Car:
+    wheels = 4
+
+car1 = Car()
+car2 = Car()
+
+print(car1.wheels)
+print(car2.wheels)
+
+
+class Person:
+    country = "Kazakhstan"
+
+person1 = Person()
+person2 = Person()
+
+print(person1.country)
+print(person2.country)
